ee34cf4f4dc954096f2828e1d4d893b8c6462911
Paxton Porenta <162552556+Paxtonpt@users.noreply.github.com>
2024-07-20 01:35:48 -0700

    Update footer.php

--- online-course-registration-master/admin/includes/footer.php
+++ online-course-registration-master/admin/includes/footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                &copy; 2023 Online Course Registration
+                &copy; July 2024 Student Registration Portal
             </div>
 
         </div>
